f34734604cb3d3cbc68cca988fc366191b8baf89
Frenda <qiba909@gmail.com>
2017-06-16 21:50:50 +0800

    完成str_to_int(df)
    完成str_to_int(df)

--- megerfiles.py
+++ megerfiles.py
@@ -2,6 +2,7 @@ import os
 import re
 from pandas import DataFrame
 import pandas as pd
+import numpy as np
 
 def enum_files(dir):
     files = [name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))]
@@ -10,8 +11,17 @@ def enum_files(dir):
 def get_date_from_file_name(file):
     return re.split(r'[_.]', file)[:2]
 
-def str_to_int():
-    
+def str_to_int(df):
+    data = []
+    pattern = re.compile(r'万')
+    for i in df.点击:
+        if i == '--':
+            data.append(np.nan)
+        elif i.find('万') != -1:
+            data.append(int(float(pattern.sub('', i)) * 10000))
+        else:
+            data.append(int(i))
+    return data
 
 def meger_files(dir):
     data = None
@@ -24,6 +34,7 @@ def meger_files(dir):
         data['年'] = date[0]
         data['月'] = date[1]
     data = data.reset_index(drop = True)
+    data.点击 = str_to_int(data)
     writer = pd.ExcelWriter(os.path.join(dir, 'output.xlsx'))
     data.to_excel(writer,'Sheet1')
     writer.save()
